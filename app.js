66ae19282f0510ee5be2f66c70454feb2c474802
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-03-03 14:28:28 +0530

    did some small changes w.r.t styles

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let graphDiv = document.querySelector("#graphDiv");
 
 submitButton.addEventListener("click", ()=>{
 	graphDivStyles();
+
 });
 
 function graphDivStyles(){
 	graphDiv.style.display = "block";
-	graphDiv.style.width = "50vw";
+	graphDiv.style.width = "45vw";
 	graphDiv.style.border = "1px solid red";
 }
+
 
