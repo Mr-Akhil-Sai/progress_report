3ed5e8514859af853d6327d79851a708c8fa0e78
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-03-04 12:28:54 +0530

    implemeted bar graph

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
 let submitButton = document.querySelector("#submit");
 let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
 
 submitButton.addEventListener("click", () => {
   canvasStyles();
+	xAxis();
+	yAxis();	
+	mathsMarks();
+	physicsMarks();
+	chemistryMarks();
+	frenchMarks();
+	englishMarks();
+	clearInputs();
 });
 
 function canvasStyles() {
   canvas.style.display = "block";
-  canvas.style.border = "1px solid red";
-  canvas.style.width = "45vw";
+  canvas.style.border = "1px solid black";
+  canvas.style.width = "50vw";
+}
+
+function xAxis(){
+	ctx.beginPath();
+	ctx.moveTo(15, 15);
+	ctx.lineTo(15, 135);
+	ctx.stroke();
+}
+
+function yAxis(){
+	ctx.beginPath();
+	ctx.moveTo(15, 135);
+	ctx.lineTo(240, 135);
+	ctx.stroke();
+}
+
+function mathsMarks(){
+	let maths = document.querySelector("#maths").value;
+	ctx.fillRect(40,135, 20, -maths);
+}
+
+function physicsMarks(){
+	let physics = document.querySelector("#physics").value;
+	ctx.fillRect(80, 135, 20, -physics);
+}
+
+function chemistryMarks(){
+	let chemistry = document.querySelector("#chemistry").value;
+	ctx.fillRect(120, 135, 20, -chemistry);
+}
+
+function frenchMarks(){
+	let french = document.querySelector("#french").value;
+	ctx.fillRect(160, 135, 20, -french);
+}
+
+function englishMarks(){
+	let english = document.querySelector("#english").value;
+	ctx.fillRect(200, 135, 20, -english);
 }
