593aceb94851891987926ebdac16d5ba1bb7ff2a
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-03-05 11:43:41 +0530

    refactoring the code

--- app.js
+++ app.js
@@ -5,26 +5,24 @@ let ctx = canvas.getContext("2d");
 let marks = document.querySelector("#marks");
 let subjects = document.querySelector("#subjects");
 let nameDiv = document.createElement("div");
+let mathsMarks = document.querySelector("#maths");
+let physicsMarks = document.querySelector("#physics");
+let chemistryMarks = document.querySelector("#chemistry");
+let frenchMarks = document.querySelector("#french");
+let englishMarks = document.querySelector("#english");
 
 submitButton.addEventListener("click", () => {
-  canvasStyles();
-  xAxis();
-  yAxis();
-  mathsMarks();
-  physicsMarks();
-  chemistryMarks();
-  frenchMarks();
-  englishMarks();
-  displayingName();
-  clearInputs();
+		canvasStyles();
+		xAxis();
+		yAxis();
+		marksDisplayingOnGraph();
+		displayingName();
+		clearInputs();	
 });
 
 clearButton.addEventListener("click", () => {
-  clearInputs();
-  canvas.style.display = "none";
-  marks.style.display = "none";
-  subjects.style.display = "none";
-  nameDiv.style.display = "none";
+	location.reload();
+	clearInputs();
 });
 
 function canvasStyles() {
@@ -38,7 +36,7 @@ function xAxis() {
   ctx.beginPath();
   ctx.moveTo(15, 15);
   ctx.lineTo(15, 135);
-  ctx.strokeStyle = "#000000";
+  ctx.strokeStyle = "black";
   ctx.stroke();
 }
 
@@ -46,32 +44,18 @@ function yAxis() {
   ctx.beginPath();
   ctx.moveTo(15, 135);
   ctx.lineTo(240, 135);
+  ctx.strokeStyle = "black";
   ctx.stroke();
 }
 
-function mathsMarks() {
-  let maths = document.querySelector("#maths").value;
-  ctx.fillRect(40, 135, 20, -maths);
+function marksDisplayingOnGraph(){
+	let marksArray = [];
+	marksArray.push(mathsMarks.value, physicsMarks.value, chemistryMarks.value, frenchMarks.value, englishMarks.value);
+	for(let i = 0; i <= marksArray.length; i++){
+		for(let j = 40; j <=200; j += 40){
+			ctx.fillRect(j, 135, 20, -marksArray[i]);
+		}
 }
-
-function physicsMarks() {
-  let physics = document.querySelector("#physics").value;
-  ctx.fillRect(80, 135, 20, -physics);
-}
-
-function chemistryMarks() {
-  let chemistry = document.querySelector("#chemistry").value;
-  ctx.fillRect(120, 135, 20, -chemistry);
-}
-
-function frenchMarks() {
-  let french = document.querySelector("#french").value;
-  ctx.fillRect(160, 135, 20, -french);
-}
-
-function englishMarks() {
-  let english = document.querySelector("#english").value;
-  ctx.fillRect(200, 135, 20, -english);
 }
 
 function displayingName() {
@@ -86,16 +70,10 @@ function displayingName() {
 
 function clearInputs() {
   let name = document.querySelector("#name");
-  let maths = document.querySelector("#maths");
-  let physics = document.querySelector("#physics");
-  let chemistry = document.querySelector("#chemistry");
-  let french = document.querySelector("#french");
-  let english = document.querySelector("#english");
-
   name.value = "";
-  maths.value = "";
-  physics.value = "";
-  chemistry.value = "";
-  french.value = "";
-  english.value = "";
+  mathsMarks.value = "";
+  physicsMarks.value = "";
+  chemistryMarks.value = "";
+  frenchMarks.value = "";
+  englishMarks.value = "";
 }
